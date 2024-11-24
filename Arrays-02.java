acdd2f3a5c3f57f77f6db42f0a5bdb2e3b7d0a59
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2024-11-24 14:24:04 +0100

    Regular 2-D Arrays

---
+++ Arrays-02.java
@@ -0,0 +1,20 @@
+// creation and traversal of an 2-D array (Regular) in Java
+
+public class MA01 {
+    public static void main(String[] args) {
+        int [][] ar = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+
+        };
+        // Traversing all the elements of an 2-D Array
+
+//        for(int i=0;i<ar.length;i++){
+//            for(int j =0;j<ar[i].length;j++){
+//                System.out.print(ar[i][j]);
+//            }
+//            System.out.println();
+//        }
+    }
+}
